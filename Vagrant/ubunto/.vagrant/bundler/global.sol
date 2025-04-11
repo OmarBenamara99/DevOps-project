4533b1fc278990436730f896292fcd7ca82cb58b
OmarBenamara99 <omar.benamara@edu.isetcom.tn>
2025-04-11 14:22:52 +0100

    devops-project

---
+++ Vagrant/ubunto/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"fb618d555598e202bde829e788aaba7d55830a2c478af6eaba58174700bb59ad","vagrant_version":"2.3.7"}
